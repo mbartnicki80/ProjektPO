00b16286c48e68bef816818fe47183595d40b218
mbartnicki80 <mbartnicki@student.agh.edu.pl>
2023-12-12 13:57:10 +0100

    added new MapDirections

--- oolab/src/main/java/agh/ics/oop/model/MapDirection.java
+++ oolab/src/main/java/agh/ics/oop/model/MapDirection.java
@@ -1,15 +1,18 @@
 package agh.ics.oop.model;
 public enum MapDirection {
-
-    NORTH, EAST, SOUTH, WEST;
+    NORTH, NORTH_EAST, EAST, SOUTH_EAST, SOUTH, SOUTH_WEST, WEST, NORTH_WEST;
 
     private static final Vector2d NORTH_VECTOR = new Vector2d(0, 1);
+    private static final Vector2d NORTH_EAST_VECTOR = new Vector2d(1, 1);
     private static final Vector2d EAST_VECTOR = new Vector2d(1, 0);
+    private static final Vector2d SOUTH_EAST_VECTOR = new Vector2d(1, -1);
     private static final Vector2d SOUTH_VECTOR = new Vector2d(0, -1);
+    private static final Vector2d SOUTH_WEST_VECTOR = new Vector2d(-1, -1);
     private static final Vector2d WEST_VECTOR = new Vector2d(-1, 0);
+    private static final Vector2d NORTH_WEST_VECTOR = new Vector2d(-1, 1);
 
     public String toString() {
-        String[] directions = {"N", "E", "S", "W"};
+        String[] directions = {"N", "NE", "E", "SE", "S", "SW", "W", "NW"};
         return directions[(this.ordinal())%(directions.length)];
     }
 
@@ -22,12 +25,22 @@ public enum MapDirection {
         MapDirection[] directions = MapDirection.values();
         return directions[(this.ordinal()+directions.length-1)%(directions.length)];
     }
+
+    public MapDirection rotate(int rotation) {
+        MapDirection[] directions = MapDirection.values();
+        return directions[(this.ordinal()+rotation)%(directions.length)];
+    }
+
     public Vector2d toUnitVector() {
         return switch (this) {
             case NORTH -> NORTH_VECTOR;
             case SOUTH -> SOUTH_VECTOR;
             case WEST -> WEST_VECTOR;
+            case NORTH_EAST -> NORTH_EAST_VECTOR;
             case EAST -> EAST_VECTOR;
+            case SOUTH_EAST -> SOUTH_EAST_VECTOR;
+            case SOUTH_WEST -> SOUTH_WEST_VECTOR;
+            case NORTH_WEST -> NORTH_WEST_VECTOR;
         };
     }
 }
--- oolab/src/test/java/agh/ics/oop/model/MapDirectionTest.java
+++ oolab/src/test/java/agh/ics/oop/model/MapDirectionTest.java
@@ -5,16 +5,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MapDirectionTest {
     @Test
     public void nextMethodTest() {
-        assertEquals(MapDirection.EAST, MapDirection.NORTH.next());
-        assertEquals(MapDirection.SOUTH, MapDirection.EAST.next());
-        assertEquals(MapDirection.WEST, MapDirection.SOUTH.next());
-        assertEquals(MapDirection.NORTH, MapDirection.WEST.next());
+        assertEquals(MapDirection.EAST, MapDirection.NORTH_EAST.next());
+        assertEquals(MapDirection.SOUTH, MapDirection.SOUTH_EAST.next());
+        assertEquals(MapDirection.WEST, MapDirection.SOUTH_WEST.next());
+        assertEquals(MapDirection.NORTH, MapDirection.NORTH_WEST.next());
+        assertEquals(MapDirection.NORTH_EAST, MapDirection.NORTH.next());
+        assertEquals(MapDirection.SOUTH_EAST, MapDirection.EAST.next());
+        assertEquals(MapDirection.SOUTH_WEST, MapDirection.SOUTH.next());
+        assertEquals(MapDirection.NORTH_WEST, MapDirection.WEST.next());
     }
     @Test
     public void previousMethodTest() {
-        assertEquals(MapDirection.EAST, MapDirection.SOUTH.previous());
-        assertEquals(MapDirection.SOUTH, MapDirection.WEST.previous());
-        assertEquals(MapDirection.WEST, MapDirection.NORTH.previous());
-        assertEquals(MapDirection.NORTH, MapDirection.EAST.previous());
+        assertEquals(MapDirection.EAST, MapDirection.SOUTH_EAST.previous());
+        assertEquals(MapDirection.SOUTH, MapDirection.SOUTH_WEST.previous());
+        assertEquals(MapDirection.WEST, MapDirection.NORTH_WEST.previous());
+        assertEquals(MapDirection.NORTH, MapDirection.NORTH_EAST.previous());
+        assertEquals(MapDirection.NORTH_EAST, MapDirection.EAST.previous());
+        assertEquals(MapDirection.SOUTH_EAST, MapDirection.SOUTH.previous());
+        assertEquals(MapDirection.SOUTH_WEST, MapDirection.WEST.previous());
+        assertEquals(MapDirection.NORTH_WEST, MapDirection.NORTH.previous());
+    }
+
+    @Test
+    public void rotateMethodTest() {
+        assertEquals(MapDirection.EAST, MapDirection.EAST.rotate(0));
+        assertEquals(MapDirection.EAST, MapDirection.NORTH_EAST.rotate(1));
+        assertEquals(MapDirection.EAST, MapDirection.NORTH.rotate(2));
+        assertEquals(MapDirection.EAST, MapDirection.NORTH_WEST.rotate(3));
+        assertEquals(MapDirection.EAST, MapDirection.WEST.rotate(4));
+        assertEquals(MapDirection.EAST, MapDirection.SOUTH_WEST.rotate(5));
+        assertEquals(MapDirection.EAST, MapDirection.SOUTH.rotate(6));
+        assertEquals(MapDirection.EAST, MapDirection.SOUTH_EAST.rotate(7));
     }
 }
